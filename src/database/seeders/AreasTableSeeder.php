ef4e0fb690d621570804cf4a118bae32fcc4058e
Takahashi <ahg945127@gmail.com>
2024-03-29 22:30:15 +0900

    areasテーブルのシーディングの作成

--- src/database/seeders/AreasTableSeeder.php
+++ src/database/seeders/AreasTableSeeder.php
@@ -13,6 +13,19 @@ class AreasTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'area_name' => '東京',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'area_name' => '大阪',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+            'area_name' => '福岡',
+        ];
+        DB::table('areas')->insert($param);
     }
 }
